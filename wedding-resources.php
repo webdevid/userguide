aa6ab0d1f04f712ee8def6cb402aba41356ab365
webdevid <dodihidayatullah@gmail.com>
2013-11-29 00:51:18 +0700

    Create wedding-resources.php

---
+++ wedding-resources.php
@@ -0,0 +1,6 @@
+
+
+http://www.wedding-invitation.co.nz/text.html
+http://bridalmusings.com/2013/11/teal-and-coral-diy-wedding-jennifer-van-elk-photography/
+http://www.weddingomania.com/luxurious-and-timeless-2014-sareh-nouri-bridal-collection/
+http://www.jennifervanelk.com/
