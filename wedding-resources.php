4e244ad06afca9a44f8e7c3594c85a052df7ee4f
webdevid <dodihidayatullah@gmail.com>
2013-11-29 10:01:33 +0700

    Update wedding-resources.php

--- wedding-resources.php
+++ wedding-resources.php
@@ -1,6 +1,15 @@
-
 http://www.wedding-invitation.co.nz/text.html
 http://bridalmusings.com/2013/11/teal-and-coral-diy-wedding-jennifer-van-elk-photography/
 http://www.weddingomania.com/luxurious-and-timeless-2014-sareh-nouri-bridal-collection/
 http://www.jennifervanelk.com/
 http://lover.ly/wedding-inspiration/Bridal+Musings
+
+
+http://www.wptavern.com/15-plugins-to-get-jetpack-functionality-without-using-jetpack
+http://wordpress.org/plugins/tiled-gallery-carousel-without-jetpack/
+http://demo.themepacific.com/plugin-tiled-gallery-carousel/2013/10/13/tiled-gallery-carousel-without-jetpack-wordpress-plugin/
+
+http://micahblu.com/demo/?theme=wp-foundation
+http://wpsessions.com/sessions/building-wordpress-plugins/
+
+http://tympanus.net/codrops/2012/08/17/creative-background-styles-and-trends-in-web-design/
